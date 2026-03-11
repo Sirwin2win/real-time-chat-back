4fd5a094df49f4421bd1931081735c985752ed69
Sirwin <sirwin2win@gmail.com>
2026-03-11 14:22:10 +0100

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.post("/register", userController.createUser);
-router.get("/login", userController.getUsers);
+router.post("/login", userController.login);
+router.get("/all", userController.getUsers);
 router.get("/:id", userController.getUserById);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
