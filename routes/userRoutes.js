240a09c82ed932e4988f5d305a43439808fe6f8a
Sirwin <sirwin2win@gmail.com>
2026-03-19 08:48:44 +0100

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.post("/register", userController.createUser);
 router.post("/login", userController.login);
 router.post("/refresh", userController.refresh);
 router.post("/logout", userController.logout);
-router.get("/all", verifyToken, userController.getUsers);
+router.get("/all", userController.getUsers);
 router.get("/:id", verifyToken, userController.getUserById);
 router.put("/:id", verifyToken, userController.updateUser);
 router.delete("/:id",verifyToken, userController.deleteUser);
